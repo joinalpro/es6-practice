a923681aa92c818ffa16a2d19c9fb2fe74e4d0ff
joinalzx <joinalzx@gmail.com>
2020-06-23 16:08:23 +0600

    this is arrow

---
+++ letconst.js
@@ -0,0 +1,21 @@
+const hubby = "Joinal Abden"
+// hubby = "Abden";
+console.log(hubby);
+
+const numbers = [12, 45]
+numbers[1] = 88;
+numbers.push(12);
+// numbers = ["Moom", "Sun","Air", "Fire"]
+const myList = {name:"joinal", phone: 34534};
+// console.log(numbers);
+
+let patientName = "Chakap";
+patientName = "Plate chamos"
+console.log(patientName);
+
+
+let sum = 0;
+for (let i = 0; i<10; i++){
+    sum = sum + 1;
+    console.log(i);
+}
---
+++ template.js
@@ -0,0 +1,24 @@
+
+// const firstName = "joinal";
+// const lastName  = "Abden";
+// const fullName = firstName + " " + lastName + " is a good boy";
+// const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+// console.log(fullName2);
+
+
+// const multiline = `I would like to say that
+// living alone is not possible but who loves his Allah. He would like to stay alone.
+// Who you are and who you want to be? `
+// console.log(multiline);
+
+
+const firstName = "Joinal";
+const lastName = "Abden";
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName} is a professional programmer. I worked ${ 18 + 25+ 149} hours.`;
+
+const multiline = "I like to say, You are a good boy.\n"
++ "very professional and have excellent work ethics.";
+const multiline3 = `So, You have learned how to write multiline. 
+this very awesome work with es6. Good Template.`
+console.log (multiline);
